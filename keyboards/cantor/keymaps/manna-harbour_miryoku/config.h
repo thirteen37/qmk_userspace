6dedc3835a0b4952ac46bc3e19c8bcd92cf0744e
Yu-Xi Lim <limyuxi@gmail.com>
2024-06-12 22:48:02 +0800

    Use U_NU instead of XXX

--- keyboards/cantor/keymaps/manna-harbour_miryoku/config.h
+++ keyboards/cantor/keymaps/manna-harbour_miryoku/config.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#define XXX KC_NO
-
 #define LAYOUT_miryoku( \
       K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09, \
       K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19, \
@@ -9,8 +7,8 @@
       N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39 \
 ) \
 LAYOUT_split_3x6_3( \
-XXX,  K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,  XXX, \
-XXX,  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  XXX, \
-XXX,  K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,  XXX , \
+U_NU,  K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,  U_NU, \
+U_NU,  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  U_NU, \
+U_NU,  K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,  U_NU , \
                   K32,  K33,  K34,         K35,  K36,  K37 \
 )
--- keyboards/keyboardio/atreus/keymaps/manna-harbour_miryoku/config.h
+++ keyboards/keyboardio/atreus/keymaps/manna-harbour_miryoku/config.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#define XXX KC_NO
-
 #define MIRYOKU_MAPPING( \
 K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
 K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
@@ -9,8 +7,8 @@ K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
 N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
 LAYOUT( \
-K00,     K01, K02, K03, K04,                   K05, K06, K07, K08, K09, \
-K10,     K11, K12, K13, K14,                   K15, K16, K17, K18, K19, \
-K20,     K21, K22, K23, K24, KC_CAPS, KC_CAPS, K25, K26, K27, K28, K29, \
-KC_CAPS, XXX, XXX, K32, K33, K34,         K35, K36, K37, XXX, XXX, KC_CAPS \
+K00,     K01,  K02,  K03, K04,                   K05, K06, K07,  K08,  K09, \
+K10,     K11,  K12,  K13, K14,                   K15, K16, K17,  K18,  K19, \
+K20,     K21,  K22,  K23, K24, KC_CAPS, KC_CAPS, K25, K26, K27,  K28,  K29, \
+KC_CAPS, U_NU, U_NU, K32, K33, K34,         K35, K36, K37, U_NU, U_NU, KC_CAPS \
 )
