09b2a472c2eea816bc89b887e1e59c0cbb5f83ad
Yu-Xi Lim <limyuxi@gmail.com>
2025-08-17 21:47:15 +0800

    Click and drag option for Ploopy trackball

--- keyboards/ploopyco/trackball_nano/keymaps/thirteen37/config.h
+++ keyboards/ploopyco/trackball_nano/keymaps/thirteen37/config.h
@@ -5,3 +5,11 @@
 
 // User-configurable key code sent when movement is detected
 #define KEY_CODE KC_F18
+
+// User-configurable key behavior mode
+// If defined, a mouse drag will be started after DRAG_DELAY when movement starts
+// If not defined, KEY_CODE will be held during movement and released after idle timeout
+#define TAP_MODE
+
+// User-configurable delay before starting mouse drag in TAP_MODE (milliseconds)
+#define DRAG_DELAY 150
--- keyboards/ploopyco/trackball_nano/keymaps/thirteen37/keymap.c
+++ keyboards/ploopyco/trackball_nano/keymaps/thirteen37/keymap.c
@@ -19,6 +19,11 @@
 
  #include QMK_KEYBOARD_H
 
+// Compile-time assertion to ensure drag delay is less than idle timeout
+#ifdef TAP_MODE
+_Static_assert(DRAG_DELAY < IDLE_TIMEOUT, "DRAG_DELAY must be less than IDLE_TIMEOUT");
+#endif
+
 // Dummy
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {{{ KC_NO }}};
 
@@ -32,13 +37,23 @@ void suspend_wakeup_init_user(void) {
 }
 
 static uint16_t idle_timer = 0;
+#ifdef TAP_MODE
+static uint16_t drag_timer = 0;
+#endif
 
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
     // if there's movement
-    if (mouse_report.x != 0 && mouse_report.y != 0) {
-        // if the idle timer is not set, send the keydown event
+    if (mouse_report.x != 0 || mouse_report.y != 0) {
+        // if the idle timer is not set, send the key event or start drag timer
         if (!idle_timer) {
+#ifdef TAP_MODE
+            // In tap mode, tap the key immediately and start the drag delay timer
+            tap_code(KEY_CODE);
+            drag_timer = timer_read();
+#else
+            // In hold mode, register (hold) the key
             register_code(KEY_CODE);
+#endif
         }
         // update the timer
         idle_timer = timer_read();
@@ -47,9 +62,23 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
 }
 
 void housekeeping_task_user(void) {
-    // if the idle timer is set and the timer has expired, send the keyup event and reset the timer
+#ifdef TAP_MODE
+    // In tap mode, start drag after delay if not already started
+    if (drag_timer && timer_elapsed(drag_timer) >= DRAG_DELAY) {
+        register_code(KC_BTN1);
+        drag_timer = 0;  // Clear timer to prevent repeated button presses
+    }
+#endif
+
+    // if the idle timer is set and the timer has expired, handle key release and reset the timer
     if (idle_timer && timer_elapsed(idle_timer) >= IDLE_TIMEOUT) {
+#ifdef TAP_MODE
+        // In tap mode, always end the mouse drag (drag must be active due to DRAG_DELAY < IDLE_TIMEOUT assertion)
+        unregister_code(KC_BTN1);
+#else
+        // In hold mode, unregister (release) the key after idle timeout
         unregister_code(KEY_CODE);
+#endif
         idle_timer = 0;
     }
 }
